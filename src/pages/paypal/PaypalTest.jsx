81d4d213a87377f3d55ca09fa95980c2f936712e
Quang Khải <khai151020@gmail.com>
2022-04-05 10:29:53 +0700

    test paypal

--- src/pages/paypal/PaypalTest.jsx
+++ src/pages/paypal/PaypalTest.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import "./paypalTest.css";
+import { PayPalButton } from "react-paypal-button-v2";
 
 function PaypalTest() {
     return (
-        <div className='paypal-test'>PaypalTest</div>
+        <div className='paypal-test'>
+            <PayPalButton
+                amount="0.01"
+                // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+                onSuccess={(details, data) => {
+                    alert("Transaction completed by " + details.payer.name.given_name);
+                    console.log(data);
+                    console.log(details);
+                    // OPTIONAL: Call your server to save the transaction
+                    return fetch("/paypal-transaction-complete", {
+                        
+                        method: "post",
+                        body: JSON.stringify({
+                        orderID: data.orderID
+                        })
+                    });
+                }}
+                createOrder = {(data, actions) => {
+                    actions.order.create()
+                }}
+            />
+        </div>
     )
 }
 
